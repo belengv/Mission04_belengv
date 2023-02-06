d948e69afc495c7ae725535a31181d9d0e1bd0db
Belen Garcia <belengv@byu.edu>
2023-02-06 10:32:04 -0700

    fixed form

--- Mission04/models/CalculatorModel.cs
+++ Mission04/models/CalculatorModel.cs
@@ -9,22 +9,22 @@ namespace Mission04_belengv.models
     public class CalculatorModel
     {
         [Required]
-        [Range(0, 100)]
+        [Range(0, 100, ErrorMessage = "Grade must be between 0 - 100!")]
         public int Assignments { get; set; }
         [Required]
-        [Range(0, 100)]
+        [Range(0, 100, ErrorMessage = "Grade must be between 0 - 100!")]
         public int Project { get; set; }
     
-        [Range(0, 100)]
+        [Range(0, 100, ErrorMessage = "Grade must be between 0 - 100!")]
         public int Quizzes { get; set; }
         [Required]
-        [Range(0, 100)]
+        [Range(0, 100, ErrorMessage = "Grade must be between 0 - 100!")]
         public int Midterm { get; set; }
         [Required]
-        [Range(0, 100)]
+        [Range(0, 100, ErrorMessage = "Grade must be between 0 - 100!")]
         public int Final { get; set; }
         [Required]
-        [Range(0, 100)]
+        [Range(0, 100, ErrorMessage = "Grade must be between 0 - 100!")]
         public int Intex { get; set; }
 
 
